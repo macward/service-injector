13593b27a070c2929a2f268fbf8f5000c4a7e018
Max <ward.maximiliano@gmail.com>
2024-04-29 03:14:07 -0300

    fix class access level

--- Sources/ServiceInjector/DependencyContainer.swift
+++ Sources/ServiceInjector/DependencyContainer.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+public enum ServiceLifecycle {
+    case singleton, runtime
+}
+
 enum ServiceLocatorError: Error {
     case serviceAlreadyRegistered(String)
     case serviceNotFound(String)
@@ -61,81 +65,88 @@ final public class ServiceLocator {
     ///
     /// This method ensures that each service is registered only once (unless explicitly overwritten)
     /// to maintain the integrity and predictability of your service dependencies.
-    public static func register<T>(as type: T.Type, identifier: String? = nil, using factory: @autoclosure @escaping () -> T) throws {
+    public static func register<T>(as type: T.Type, withLifecycle lifecycle: ServiceLifecycle, identifier: String? = nil, using factory: @autoclosure @escaping () -> T) throws {
         let key = identifier ?? String(describing: type.self)
         guard shared.serviceFactories[key] == nil else {
             throw ServiceLocatorError.serviceAlreadyRegistered(key)
         }
         shared.serviceFactories[key] = factory
+        if lifecycle == .singleton {
+            shared.cache[key] = factory()
+        }
     }
     
-    /// Overrides a previously registered service with a new factory closure.
+    /// Overrides a previously registered service with a new factory closure, considering the specified lifecycle.
     ///
-    /// Use this method to replace an existing service registration with a new implementation.
-    /// This can be useful in scenarios where you need to change the behavior of a service
-    /// dynamically at runtime, or for replacing real services with mock versions in testing environments.
+    /// Use this method to replace an existing service registration with a new implementation, while also specifying the service's lifecycle (singleton or runtime).
+    /// This can be useful in scenarios where you need to change the behavior of a service dynamically at runtime,
+    /// or for replacing real services with mock versions in testing environments.
     ///
-    /// The method replaces the existing factory closure for the specified service type and identifier
-    /// with a new one provided. If the service was previously instantiated and cached, the cache is cleared,
-    /// ensuring that the next request for the service will use the new factory closure to create an instance.
+    /// The method replaces the existing factory closure for the specified service type, identifier, and lifecycle
+    /// with a new one provided. If the service was previously instantiated and cached as a singleton, the cache is cleared,
+    /// ensuring that the next request for the service will use the new factory closure to create an instance, according to the specified lifecycle.
     ///
     /// - Parameters:
-    ///   - type: The type of the service to override. This should be a protocol or superclass
-    ///           that the service conforms to or inherits from.
-    ///   - identifier: An optional unique identifier for the service. Use this if you have multiple
-    ///                 instances of the same service type registered and need to distinguish between them
-    ///                 for replacement.
-    ///   - factory: A closure that returns an instance of the new service. This closure is executed
-    ///              the next time the service is requested, allowing for lazy initialization of the new service.
+    ///   - type: The type of the service to override. This should be a protocol or superclass that the service conforms to or inherits from.
+    ///   - lifecycle: The lifecycle of the service (`singleton` or `runtime`), which determines how the service instance should be managed.
+    ///   - identifier: An optional unique identifier for the service. Use this if you have multiple instances of the same service type registered and need to distinguish between them for replacement.
+    ///   - factory: A closure that returns an instance of the new service. This closure is executed the next time the service is requested, allowing for lazy initialization of the new service according to its lifecycle.
     ///
-    /// - Note: If no service has been previously registered with the given type (and identifier, if provided),
-    ///         this method will simply register the new service as if calling `register(as:identifier:using:)`.
+    /// - Note: If no service has been previously registered with the given type, identifier, and lifecycle, this method will simply register the new service as if calling `register(as:withLifecycle:identifier:using:)`.
     ///
     /// - Example:
     ///
     /// ```
-    /// // Initial registration of a service
-    /// ServiceLocator.register(as: MyServiceProtocol.self) {
+    /// // Initial registration of a service as a singleton
+    /// ServiceLocator.register(as: MyServiceProtocol.self, withLifecycle: .singleton) {
     ///     RealService()
     /// }
     ///
-    /// // Later in the application lifecycle, override the service with a mock version
-    /// ServiceLocator.override(as: MyServiceProtocol.self) {
+    /// // Later in the application lifecycle, override the service with a mock version, still as a singleton
+    /// ServiceLocator.override(as: MyServiceProtocol.self, withLifecycle: .singleton, identifier: nil) {
     ///     MockService()
     /// }
     /// ```
     ///
-    /// This approach allows for flexible and dynamic replacement of services,
-    /// facilitating easy testing and development of different components of your application.
-    public static func override<T>(as type: T.Type, identifier: String? = nil, using factory: @autoclosure @escaping () -> T) {
+    /// This approach allows for flexible and dynamic replacement of services, facilitating easy testing and development of different
+    ///  components of your application, while respecting the specified lifecycle of the services.
+    public static func override<T>(as type: T.Type, withLifecycle lifecycle: ServiceLifecycle, identifier: String? = nil, using factory: @autoclosure @escaping () -> T) {
         let key = identifier ?? String(describing: type.self)
+        
+        // Update the service factory with the new factory closure.
         shared.serviceFactories[key] = factory
-        shared.cache.removeValue(forKey: key)
+        
+        // If the service is a singleton and already instantiated, clear it from the cache to ensure the new factory is used next time.
+        if lifecycle == .singleton {
+            shared.cache.removeValue(forKey: key)
+        }
+        // Note: For runtime services, there's no need to clear the cache as they are not cached.
     }
     
-    /// Locates and returns an instance of the requested service.
+    /// Locates and returns an instance of the requested service, managing its lifecycle accordingly.
     ///
-    /// This method attempts to find a service that matches the specified type and optional identifier.
-    /// If a service is found in the cache, it is returned immediately to improve performance.
-    /// If the service is not found in the cache, it attempts to create a new instance using the factory closure
-    /// provided during registration. The newly created service instance is then cached for future use.
+    /// This method attempts to find a service that matches the specified type, lifecycle, and optional identifier.
+    /// If the service's lifecycle is `.singleton` and it is found in the cache, it is returned immediately to improve performance.
+    /// If the service is not found in the cache or its lifecycle is `.runtime`, it attempts to create a new instance using the factory closure
+    /// provided during registration. For singleton services, the newly created service instance is then cached for future use.
     ///
-    /// If no service matching the type and identifier is registered, this method throws an error.
+    /// If no service matching the type, lifecycle, and identifier is registered, this method throws an error.
     ///
     /// - Parameters:
     ///   - type: The type of the service to locate. It must conform to the expected protocol or class type.
+    ///   - lifecycle: The lifecycle of the service (`singleton` or `runtime`), which determines how the service instance is managed.
     ///   - identifier: An optional unique identifier for the service. Use this if you have multiple instances
     ///                 of the same service type registered and need to distinguish between them.
     ///
     /// - Returns: An instance of the requested service.
     ///
-    /// - Throws: `ServiceLocatorError.serviceNotFound` if no service matching the type and identifier is found.
+    /// - Throws: `ServiceLocatorError.serviceNotFound` if no service matching the type, lifecycle, and identifier is found.
     ///
     /// - Example:
     ///
     /// ```
     /// do {
-    ///     let service: MyServiceProtocol = try ServiceLocator.locateService(ofType: MyServiceProtocol.self)
+    ///     let service: MyServiceProtocol = try ServiceLocator.locateService(ofType: MyServiceProtocol.self, withLifecycle: .singleton)
     ///     service.performAction()
     /// } catch ServiceLocatorError.serviceNotFound(let serviceName) {
     ///     print("Service \(serviceName) not found.")
@@ -143,17 +154,35 @@ final public class ServiceLocator {
     ///     print("Unexpected error: \(error).")
     /// }
     /// ```
-    public static func locateService<T>(ofType type: T.Type, withIdentifier identifier: String? = nil) throws -> T {
+    ///
+    /// This approach allows you to control the lifecycle of the services being retrieved,
+    /// ensuring that you can manage service instances according to your application's needs,
+    /// whether they need to be unique and persist for the application's lifetime (singleton)
+    /// or should be created anew for each use (runtime).
+
+    public static func locateService<T>(ofType type: T.Type, withLifecycle lifecycle: ServiceLifecycle, withIdentifier identifier: String? = nil) throws -> T {
         let key = identifier ?? String(describing: type.self)
-        if let cached = shared.cache[key] as? T {
-            return cached
+
+        switch lifecycle {
+        case .singleton:
+            if let cached = shared.cache[key] as? T {
+                return cached
+            }
+            guard let service = shared.serviceFactories[key]?() as? T else {
+                throw ServiceLocatorError.serviceNotFound(key)
+            }
+            shared.cache[key] = service
+            return service
+
+        case .runtime:
+            guard let service = shared.serviceFactories[key]?() as? T else {
+                throw ServiceLocatorError.serviceNotFound(key)
+            }
+            // Para servicios con ciclo de vida 'runtime', no almacenamos la instancia en cache.
+            return service
         }
-        guard let service = shared.serviceFactories[key]?() as? T else {
-            throw ServiceLocatorError.serviceNotFound(key)
-        }
-        shared.cache[key] = service
-        return service
     }
+
     
     /// Clears the cached instances of services.
     public static func clearCache() {
@@ -167,7 +196,6 @@ final public class ServiceLocator {
     ///   - identifier: An optional unique identifier for the service.
     public static func unregister<T>(type: T.Type, identifier: String? = nil) {
         let key = identifier ?? String(describing: type.self)
-        // Elimina la fábrica del servicio y cualquier instancia en caché.
         shared.serviceFactories.removeValue(forKey: key)
         shared.cache.removeValue(forKey: key)
     }
--- Sources/ServiceInjector/ServiceInjector.swift
+++ Sources/ServiceInjector/ServiceInjector.swift
@@ -28,25 +28,22 @@
 /// In the example above, `myService` and `specificService` are automatically injected
 /// with the instances registered in `ServiceLocator` by their type and optional identifier.
 @propertyWrapper
-struct Inject<T> {
+public struct Inject<T> {
     /// The instance of the service that is being injected.
     private var service: T
     
-    /// An optional identifier to locate a specific service instance if multiple
-    /// instances of the same type are registered in `ServiceLocator`.
-    private var identifier: String?
-
-    /// Initializes a new injectable service instance.
+    /// Initializes a new injectable service instance with an optional lifecycle and identifier.
     ///
     /// The initializer looks up and retrieves the service from `ServiceLocator`.
     /// If an identifier is provided, it attempts to locate the service using that identifier.
     ///
-    /// - Parameter identifier: An optional unique identifier for the service.
-    init(identifier: String? = nil) {
-        self.identifier = identifier
-        // Locate and cache the service using ServiceLocator.
+    /// - Parameters:
+    ///   - lifecycle: The lifecycle of the service (e.g., `.singleton` or `.runtime`).
+    ///   - identifier: An optional unique identifier for the service.
+
+    public init(_ lifecycle: ServiceLifecycle = .runtime, identifier: String? = nil) {
         do {
-            self.service = try ServiceLocator.locateService(ofType: T.self, withIdentifier: identifier)
+            self.service = try ServiceLocator.locateService(ofType: T.self, withLifecycle: lifecycle, withIdentifier: identifier)
         } catch {
             fatalError(error.localizedDescription)
         }
@@ -56,7 +53,7 @@ struct Inject<T> {
     ///
     /// This property provides access to the injected service instance. It allows
     /// the consumer of the property wrapper to interact with the service directly.
-    var wrappedValue: T {
+    public var wrappedValue: T {
         get { service }
         mutating set { service = newValue }
     }
